b25a35bae450c2efcec2eb70ea21f8c7fac25dda
abel_m <me.abel325@gmail.com>
2022-11-14 17:43:13 +0200

    Create function to make operations

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function multiply(x, y){
 function divide(x, y){
     return x / y;
 }
+
+function operate(operator, x, y){
+    if(operator === '+') return add(x, y);
+    if(operator === '-') return subtract(x, y);
+    if(operator === '*') return multiply(x, y);
+    if(operator === '/') return divide(x, y); 
+    return "Wrong Operator";
+}
